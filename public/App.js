226660e40d52e74e6b33d8cb29df0741fee4ef32
Ida Akiwumi <618307+IdaAkiwumi@users.noreply.github.com>
2021-05-23 14:04:09 -0400

    name

--- public/App.js
+++ public/App.js
@@ -4,12 +4,15 @@ var display2 = document.querySelector('#displayt');
 var nameButton = document.querySelector('#nameid');
 var skillButton = document.querySelector('#skillid');
 
+
+display2.textContent ='My name is Ida';
+
 function App() {
 
 
 function clickedName(){
 
-display2.textContent ='I am a Front-End Developer';
+display2.textContent ='My name is Ida';
 }
 
 nameButton.addEventListener("click", clickedName);
