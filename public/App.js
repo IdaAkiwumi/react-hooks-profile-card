7c3878dd998dc8dba6d5e379e3e7b5f7b7fa95a7
Ida Akiwumi <618307+IdaAkiwumi@users.noreply.github.com>
2021-05-23 10:53:05 -0400

    button

--- public/App.js
+++ public/App.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import './responsive.css';
 import './styles.css';
 
 var display2 = document.querySelector('#skill');
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+var mainButton = document.querySelector('.skills');
 
 function clickedButton(){
 
@@ -34,6 +14,6 @@ mainButton.addEventListener("click", clickedButton);
 
 
 
-}
+
 
 export default App;
