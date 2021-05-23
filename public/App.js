7ea38dd8fbc458637110891ea2f1e81aa41bf34b
Ida Akiwumi <618307+IdaAkiwumi@users.noreply.github.com>
2021-05-23 11:00:43 -0400

    n

--- public/App.js
+++ public/App.js
@@ -1,19 +1,30 @@
-import './responsive.css';
+import 'responsive.css';
 import './styles.css';
 
-var display2 = document.querySelector('#skill');
-var mainButton = document.querySelector('.skills');
+var display2 = document.querySelector('#displayt');
+var nameButton = document.querySelector('.name');
+var skillButton = document.querySelector('.skills');
 
-function clickedButton(){
+function clickedName(){
 
 display2.textContent ='I am a Front-End Developer';
 }
 
-mainButton.addEventListener("click", clickedButton);
+skillButton.addEventListener("click", clickedName);
+
+
+
+
+function clickedSkills(){
+
+display2.textContent ='I am a Front-End Developer';
+}
+
+skillButton.addEventListener("click", clickedSkills);
 
 
 
 
 
-
-export default App;
+export default clickedName;
+export default clickedSkills;
