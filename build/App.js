9814619af6b02b78e0653a1b40d77567ab0d5e51
Ida Akiwumi <618307+IdaAkiwumi@users.noreply.github.com>
2021-05-23 20:43:07 -0400

    n

---
+++ build/App.js
@@ -0,0 +1,48 @@
+
+var allElements= document.querySelector('.container');
+var display2 = document.querySelector('#displayt');
+var nameButton = document.querySelector('#nameid');
+var skillButton = document.querySelector('#skillid');
+var counternum = document.querySelector('.counternum');
+var like = document.querySelector('#likeid');
+
+display2.textContent ='My name is Ida';
+
+
+
+
+function clickedName(){
+
+display2.textContent ='My name is Ida';
+}
+
+nameButton.addEventListener("click", clickedName);
+
+
+
+
+function clickedSkills(){
+
+display2.textContent='I am a Front-End Developer';
+}
+
+skillButton.addEventListener("click", clickedSkills);
+
+
+
+var numVal = counternum.nodeValue= 0;
+
+counternum.textContent = numVal;
+
+var number=1;
+
+function heartClick(){
+
+  var i = numVal + number;
+    number++;
+  counternum.textContent=i;
+
+}
+
+like.addEventListener("click", heartClick);
+
